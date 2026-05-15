8f8c4797885d27dd4c8a6feca03202e0eae39388
mukesh.pyla <mukesh.pyla@coditas.com>
2026-05-15 10:38:22 +0530

    From branch b1 second commit

--- src/main/java/com/example/courseManagementProject/controller/Test.java
+++ src/main/java/com/example/courseManagementProject/controller/Test.java
@@ -3,5 +3,6 @@ package com.example.courseManagementProject.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("First Line");
+        System.out.println("Second line from branch1");
     }
 }
